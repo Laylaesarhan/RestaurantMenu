aa61a35434e227bc22c16e70b8d6c75cd8733443
Layla <laylaesarhan@gmail.com>
2021-09-13 19:56:46 -0500

    updates

--- src/Restaurant/Menu.java
+++ src/Restaurant/Menu.java
@@ -4,16 +4,25 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Menu {
-    private Boolean isNew;
     private String dateUpdated;
 //    public ArrayList<MenuItems> menuItems;
-   ArrayList<MenuItems> foodItems = new ArrayList<>();
+
+    public Menu(String date) {
+        this.dateUpdated = date;
+    }
+
+   public ArrayList<MenuItems> foodItems = new ArrayList<>();
 
    void addItem() {
        foodItems.add();
-       dateUpdated = Date();
    }
 
+    public String getDateUpdated() {
+        return dateUpdated;
+    }
 
+    private void updatingDate() {
+       if (foodItems )
 
+    }
 }
--- src/Restaurant/MenuItems.java
+++ src/Restaurant/MenuItems.java
@@ -1,33 +1,68 @@
 package Restaurant;
 
 public class MenuItems {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+    }
 
+    private String itemName;
     private Float itemPrice;
     private String itemDescription;
     private String itemCategory;
+    private boolean isNew;
 
-        public MenuItems (Float price, String description, String category) {
-            this.itemPrice = price;
-            this.itemDescription = description;
-            this.itemCategory = category;
+    public MenuItems(String name, Float price, String description, String category, boolean isNew) {
+        this.itemName = name;
+        this.itemPrice = price;
+        this.itemDescription = description;
+        this.itemCategory = category;
+        this.isNew = isNew;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public Float getItemPrice() {
+        return itemPrice;
+    }
+
+    public void setItemPrice(Float itemPrice) {
+        this.itemPrice = itemPrice;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
+
+    public String getItemCategory() {
+        return itemCategory;
+    }
+
+    public void setItemCategory(String itemCategory) {
+        this.itemCategory = itemCategory;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
+
+    private String newItem() {
+        if(this.isNew) {
+            return ("This is new.");
+        } else {
+            return ("This is not new.");
         }
-        public Float getItemPrice () {
-            return this.itemPrice;
-        }
-        public void setItemPrice (Float aPrice) {
-            this.itemPrice = aPrice;
-    }
-        public String getItemDescription () {
-        return this.itemDescription;
-    }
-        public void setItemDescription (String aDescription) {
-        this.itemDescription = aDescription;
-    }
-        public String getItemCategory () {
-        return this.itemCategory;
-    }
-        public void setItemCategory (String aCatergory) {
-        this.itemCategory = aCatergory;
     }
 }
